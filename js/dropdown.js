1fccb2a581dbec26c01c2fd81012b55244fc7535
Ho-oh <ohou@nyit.edu>
2022-12-14 00:45:29 -0500

    added recorded demo

---
+++ js/dropdown.js
@@ -0,0 +1,31 @@
+const dropDemoOne = () => {
+    document.getElementById("demo-one").classList.toggle("show");
+}
+
+const dropDemoTwo = () => {
+  document.getElementById("demo-two").classList.toggle("show");
+}
+
+const dropDemoThree = () => {
+  document.getElementById("demo-three").classList.toggle("show");
+}
+
+const dropDemoFour = () => {
+  document.getElementById("demo-four").classList.toggle("show");
+}
+
+const dropDemoFive = () => {
+  document.getElementById("demo-five").classList.toggle("show");
+}
+
+const dropDemoSix = () => {
+  document.getElementById("demo-six").classList.toggle("show");
+}
+
+const dropDemoSeven = () => {
+  document.getElementById("demo-seven").classList.toggle("show");
+}
+
+const dropDemoEight = () => {
+  document.getElementById("demo-eight").classList.toggle("show");
+}
